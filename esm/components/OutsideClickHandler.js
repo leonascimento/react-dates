d5abc08cecc77e7f5be1b4a6898cdaf7bb764326
Leonardo Nascimento <leo386386@gmail.com>
2018-06-07 10:20:46 -0300

    Temporary build of children feature.

---
+++ esm/components/OutsideClickHandler.js
@@ -0,0 +1,5 @@
+import OutsideClickHandler from 'react-outside-click-handler';
+
+console.warn('\n  The \'react-dates\' OutsideClickHandler export is deprecated.\n  Please install and import from \'react-outside-click-handler\' instead.\n');
+
+export default OutsideClickHandler;
